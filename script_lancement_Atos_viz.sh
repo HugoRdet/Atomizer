27c095e1d67d47b19ca5e609420430d6a0963bbe
Hugo Riffaud De <hugordet@gmail.com>
2026-05-21 17:08:46 +0200

    sanity check

--- script_lancement_Atos_viz.sh
+++ script_lancement_Atos_viz.sh
@@ -20,5 +20,5 @@ conda activate venv
 #python3 flair_test.py
 #sh TrainEval_MAE.sh "$EXPERIMENT_NAME" "$MODEL_NAME" regular
 
-#python3 script_train_baselines_flairhub.py --xp_name vit_pm_v1_spot_train --model vit_pm  --subset_indices ./data/FLAIR-HUB/subset_indices.json --use_vhr false --use_spot true --spot_norm_as_vhr false --use_dem true --use_s2 true --use_s1 true --multi_temporal 6 --epochs 30
-python3 script_train_baselines_flairhub.py --xp_name resnet_concat_v1_spot_train --model resnet --resnet_variant resnet50 --subset_indices ./data/FLAIR-HUB/subset_indices.json --use_vhr false --use_spot true  --spot_norm_as_vhr false  --use_dem true --use_s2 true --use_s1 true --multi_temporal 6 --epochs 30
+python3 script_train_baselines_flairhub.py --xp_name vit_pm_v1_spot_train --model vit_pm  --subset_indices ./data/FLAIR-HUB/subset_indices.json --use_vhr false --use_spot true --spot_norm_as_vhr false --use_dem true --use_s2 true --use_s1 true --multi_temporal 6 --epochs 30
+#python3 script_train_baselines_flairhub.py --xp_name resnet_concat_v1_spot_train --model resnet --resnet_variant resnet50 --subset_indices ./data/FLAIR-HUB/subset_indices.json --use_vhr false --use_spot true  --spot_norm_as_vhr false  --use_dem true --use_s2 true --use_s1 true --multi_temporal 6 --epochs 30
